11e35b95147475e4acc3a755392b9703fee6b699
Valentin Waeselynck <val@bandsquare.fr>
2014-06-15 21:02:38 +0200

    implemented best feature choice

--- src/chapters/trees.clj
+++ src/chapters/trees.clj
@@ -10,6 +10,14 @@
         clojure.pprint)
   )
 
+(defn create-dataset []
+  {:labels [:no-surfacing, :flippers]
+   :dataset [[1 1 :yes]
+             [1 1 :yes]
+             [1 0 :no]
+             [0 1 :no]
+             [0 1 :no]]})
+
 (def calc-Shannon-entropy 
   (let [lg2 (java.lang.Math/log 2)
         -xlogx (fn [x] (* (- x) (/ (java.lang.Math/log x) lg2)))]
@@ -28,3 +36,29 @@
                          [1 0 :no]
                          [0 1 :no]
                          [0 1 :no]]) => 0.9709505944546686)
+
+(defn- split-on  [feat-idx dataset]
+  (->> dataset (group-by #(nth % feat-idx) ) 
+    (algo/transform-vals vec)))
+
+(defn choose-best-feature-to-split [dataset & [num-features]]
+  (let [num-features (or num-features (-> dataset first count (- 1)))
+        get-label #(nth % num-features)
+        size (count dataset)
+        
+        base-entropy (calc-Shannon-entropy get-label dataset)
+        entropy-of-split (fn [split] 
+                           (reduce (fn [ent sub-dataset]
+                                    (let [prob (-> sub-dataset count double (/ size))]
+                                      (+ ent (* prob (calc-Shannon-entropy get-label sub-dataset)))
+                                      ))
+                                  0.0 (vals split)))
+        info-gain-on-feat (fn [feat-idx] (- base-entropy
+                                            (entropy-of-split (split-on feat-idx dataset)) ))
+        ]
+    (algo/argmax info-gain-on-feat (range num-features))
+    ))
+(comment 
+  (choose-best-feature-to-split
+    (:dataset (create-dataset)) 2) => 0
+  )
